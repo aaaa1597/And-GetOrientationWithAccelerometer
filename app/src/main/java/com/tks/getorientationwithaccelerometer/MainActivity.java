e9b2c46c44924426f72d8d56cbd87b270a15bf16
aaaaa <aaaaa>
2023-02-15 21:51:46 +0900

    できた。　ちょくちょく、がくがくするけど、まーそこはいいや。原因は分っとーし。

--- app/src/main/java/com/tks/getorientationwithaccelerometer/MainActivity.java
+++ app/src/main/java/com/tks/getorientationwithaccelerometer/MainActivity.java
@@ -8,11 +8,21 @@ import android.hardware.SensorEventCallback;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
 import android.view.Surface;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Locale;
+import java.util.function.BiFunction;
+import java.util.function.IntFunction;
+
+import kotlin.jvm.functions.Function1;
 
 public class MainActivity extends AppCompatActivity {
     private SensorManager mSensorManager;
+    private int mPrevRotation = -1;
     private int mRotation = Surface.ROTATION_0;
 
     @Override
@@ -67,6 +77,51 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("aaaaa", "横↑向き:" + mRotation);
             else if(mRotation == Surface.ROTATION_90)
                 Log.d("aaaaa", "横↓向き:" + mRotation);
+
+            IntFunction<String> getMsg = (rot) -> {
+                if(rot == Surface.ROTATION_0)        return "上向き";
+                else if(rot == Surface.ROTATION_180) return "↓向き";
+                else if(rot == Surface.ROTATION_270) return "横↑向き";
+                else if(rot == Surface.ROTATION_90)  return "横↓向き";
+                return "none";
+            };
+
+            /* 開始角度>終了角度の場合、反時計回りに回転します */
+            Function1<Pair<Integer,Integer>, Pair<Integer,Integer>> getDegrees = (rots) -> {
+                if(     rots.first == Surface.ROTATION_0  && rots.second == Surface.ROTATION_90 )  return new Pair<>(360, 270);/*"上向き"->"横↓向き"*/
+                else if(rots.first == Surface.ROTATION_0  && rots.second == Surface.ROTATION_180)  return new Pair<>(  0, 180);/*"上向き"->  "↓向き"*/
+                else if(rots.first == Surface.ROTATION_0  && rots.second == Surface.ROTATION_270)  return new Pair<>(  0,  90);/*"上向き"->"横↑向き"*/
+
+                else if(rots.first == Surface.ROTATION_90  && rots.second == Surface.ROTATION_0 )  return new Pair<>( 270,360);   /*"横↓向き"->  "上向き"*/
+                else if(rots.first == Surface.ROTATION_90  && rots.second == Surface.ROTATION_180) return new Pair<>( 270,180);   /*"横↓向き"->  "↓向き"*/
+                else if(rots.first == Surface.ROTATION_90  && rots.second == Surface.ROTATION_270) return new Pair<>( 270, 90);   /*"横↓向き"->"横↑向き"*/
+                                                                                                                                        
+                else if(rots.first == Surface.ROTATION_180 && rots.second == Surface.ROTATION_0 )  return new Pair<>( 180, 0);   /*"↓向き"->  "上向き"*/
+                else if(rots.first == Surface.ROTATION_180 && rots.second == Surface.ROTATION_90)  return new Pair<>( 180,270);   /*"↓向き"->"横↓向き"*/
+                else if(rots.first == Surface.ROTATION_180 && rots.second == Surface.ROTATION_270) return new Pair<>( 180, 90);   /*"↓向き"->"横↑向き"*/
+                                                                                                                                        
+                else if(rots.first == Surface.ROTATION_270 && rots.second == Surface.ROTATION_0 )  return new Pair<>( 90,  0);   /*"横↑向き"->  "上向き"*/
+                else if(rots.first == Surface.ROTATION_270 && rots.second == Surface.ROTATION_90)  return new Pair<>( 90,270);   /*"横↑向き"->"横↓向き"*/
+                else if(rots.first == Surface.ROTATION_270 && rots.second == Surface.ROTATION_180) return new Pair<>( 90,180);   /*"横↑向き"->  "↓向き"*/
+
+                return new Pair<>(0,0);
+            };
+
+            if(mPrevRotation == -1) {
+                mPrevRotation = mRotation;
+                return;
+            }
+            if(mPrevRotation != mRotation) {
+                Pair<Integer, Integer> dstrot = getDegrees.invoke(new Pair<>(mPrevRotation, mRotation));
+                String logstr = String.format(Locale.JAPAN, "%s(%d, %d°) -> %s(%d, %d°)", getMsg.apply(mPrevRotation), mPrevRotation, dstrot.first, getMsg.apply(mRotation), mRotation, dstrot.second);
+                Log.d("aaaaa", logstr);
+                ImageView imgrabit = findViewById(R.id.img_rabit);
+                RotateAnimation rotanim = new RotateAnimation(dstrot.first, dstrot.second, imgrabit.getPivotX(), imgrabit.getPivotY());
+                rotanim.setDuration(1000);
+                rotanim.setFillAfter(true);
+                findViewById(R.id.img_rabit).startAnimation(rotanim);
+                mPrevRotation = mRotation;
+            }
         }
     };
 }
